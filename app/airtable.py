c049381553bfcf7a6a766032a333ab03257ab4bd
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-07-29 12:59:29 +0200

    cache airtable responses

--- app/airtable.py
+++ app/airtable.py
@@ -1,7 +1,9 @@
+import json
 import os
 from dataclasses import dataclass
-from typing import Optional, Sequence
+from typing import Callable, Optional, Sequence, TypeVar, cast
 
+import redis
 from pyairtable import Api
 from pyairtable.api.types import RecordDict
 
@@ -13,27 +15,64 @@ class AirtableConfig:
     table_ids: dict[str, str]
 
 
+@dataclass
+class RedisCacheConfig:
+    redis_url: str
+    prefix: str
+    timeout: int
+
+
+T = TypeVar("T")
+
+
+class RedisCache:
+    def __init__(self, config: RedisCacheConfig):
+        self.config = config
+        self.redis = redis.from_url(config.redis_url)
+
+    def cached(
+        self, prefix: str, args: list[str], func: Callable[..., T]
+    ) -> T:
+        key = self._get_key(prefix, args)
+        cached_value = self.redis.get(key)
+        if cached_value is not None:
+            return cast(T, json.loads(cached_value))
+        else:
+            value = func(*args)
+            self.redis.set(key, json.dumps(value))
+            self.redis.expire(key, self.config.timeout)
+            return value
+
+    def _get_key(self, prefix: str, args: list[str]) -> str:
+        return "_".join([self.config.prefix, prefix] + args)
+
+
 class AirtableDB:
-    def __init__(self, airtable_config: AirtableConfig):
-        self.airtable_config = airtable_config
-        self.api = Api(airtable_config.token)
+    def __init__(self, config: AirtableConfig, cache: RedisCache):
+        self.config = config
+        self.cache = cache
+        self.api = Api(config.token)
 
     def all(self, table_name: str) -> Sequence[RecordDict]:
-        table_id = self._table_id(table_name)
-        if table_id:
-            return self.api.table(self.airtable_config.base_id, table_id).all()
-        return []
+        return self.cache.cached("all", [table_name], self._uncached_all)
 
     def get(self, table_name: str, id: str) -> Optional[RecordDict]:
+        return self.cache.cached("get", [table_name, id], self._uncached_get)
+
+    def _uncached_all(self, table_name: str) -> Sequence[RecordDict]:
         table_id = self._table_id(table_name)
         if table_id:
-            return self.api.table(self.airtable_config.base_id, table_id).get(
-                id
-            )
+            return self.api.table(self.config.base_id, table_id).all()
+        return []
+
+    def _uncached_get(self, table_name: str, id: str) -> Optional[RecordDict]:
+        table_id = self._table_id(table_name)
+        if table_id:
+            return self.api.table(self.config.base_id, table_id).get(id)
         return None
 
     def _table_id(self, table_name: str) -> Optional[str]:
-        return self.airtable_config.table_ids.get(table_name)
+        return self.config.table_ids.get(table_name)
 
 
 def get_db_instance() -> AirtableDB:
@@ -50,4 +89,13 @@ def get_db_instance() -> AirtableDB:
             "ui_strings": os.environ["AIRTABLE_UI_STRINGS_TABLE_ID"],
         },
     )
-    return AirtableDB(airtable_config)
+
+    cache_config = RedisCacheConfig(
+        redis_url=os.environ["REDIS_URL"],
+        timeout=int(os.environ["REDIS_CACHE_TIMEOUT"]),
+        prefix=os.environ["REDIS_CACHE_PREFIX"],
+    )
+
+    cache = RedisCache(cache_config)
+
+    return AirtableDB(airtable_config, cache)
