a907358aece6832320460b6d90a69f23ee4c6c7d
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-07-29 08:56:34 +0200

    get image urls and format types from db

--- tests/airtable_db_test.py
+++ tests/airtable_db_test.py
@@ -15,64 +15,77 @@ class TestAirtableDocumentDatabase:
         self.airtable_client = self.airtable_constructor.return_value
         self.documents_table = MagicMock()
         self.themes_table = MagicMock()
-        self.themes_table.get.side_effect = lambda arg: self.themes.get(arg)
+        self.formats_table = MagicMock()
+        self.featured_documents_table = MagicMock()
+        self.ui_strings_table = MagicMock()
+        self.formats_table.get.side_effect = lambda arg: self.formats.get(arg)
         self.airtable_client.table.side_effect = [
             self.documents_table,
             self.themes_table,
+            self.formats_table,
+            self.featured_documents_table,
+            self.ui_strings_table,
+        ]
+        self.themes_table.all.return_value = [
+                {"id": id, "fields": fields} for (id, fields) in self.themes.items()
         ]
         self.documents_table.all.return_value = [
             {"fields": document} for document in self.documents
         ]
-        self.themes_table = self.themes_table
 
     def setup_method(self, method):
         self.token = "TOKEN"
         self.base_id = "BASE_ID"
         self.documents_table_id = "DOCUMENTS_TABLE_ID"
         self.themes_table_id = "THEMES_TABLE_ID"
+        self.formats_table_id = "FORMATS_TABLE_ID"
+        self.featured_documents_table_id = "FEATURED_DOCUMENTS_TABLE_ID"
+        self.ui_strings_table_id = "UI_STRINGS_TABLE_ID"
         self.themes = {
-            "theme1_id": {
-                "fields": {"name": "theme1", "summary": "Theme 1 summary"},
-            },
-            "theme2_id": {
-                "fields": {"name": "theme2", "summary": "Theme 2 summary"}
-            },
+            "theme1_id": {"name": "theme1", "summary": "Theme 1 summary"},
+            "theme2_id": {"name": "theme2", "summary": "Theme 2 summary"}
+        }
+        self.formats = {
+            "format_id": {"fields": {"name": "Format", "type": "type"}}
         }
         self.documents = [
             {
                 "link": "doc1",
                 "themes": ["theme1_id"],
                 "tags": ["tag1", "tag2"],
+                "format": ["format_id"],
             },
             {
                 "link": "doc2",
                 "themes": ["theme2_id"],
                 "tags": ["tag2", "tag3"],
+                "format": ["format_id"],
             },
         ]
+
+    def create_db(self):
+        return AirtableDocumentDatabase(
+            self.token,
+            self.base_id,
+            self.documents_table_id,
+            self.themes_table_id,
+            self.formats_table_id,
+            self.featured_documents_table_id,
+            self.ui_strings_table_id,
+        )
 
     def test_airtable_api_constructed_with_passed_token(self):
         """
         It passes the given token to the airtable constructor.
         """
-        AirtableDocumentDatabase(
-            self.token,
-            self.base_id,
-            self.documents_table_id,
-            self.themes_table_id,
-        )
+        self.create_db()
         self.airtable_constructor.assert_called_with(self.token)
 
     def test_airtable_api_gets_documents_table(self):
         """
         It gets the documents table with the given base_id and table_id
         """
-        AirtableDocumentDatabase(
-            self.token,
-            self.base_id,
-            self.documents_table_id,
-            self.themes_table_id,
-        )
+        self.create_db()
         calls = self.airtable_client.table.call_args_list
         assert call(self.base_id, self.documents_table_id) in calls
 
@@ -80,12 +93,7 @@ class TestAirtableDocumentDatabase:
         """
         It gets the themes table with the given base_id and table_id
         """
-        AirtableDocumentDatabase(
-            self.token,
-            self.base_id,
-            self.documents_table_id,
-            self.themes_table_id,
-        )
+        self.create_db()
         calls = self.airtable_client.table.call_args_list
         assert call(self.base_id, self.themes_table_id) in calls
 
@@ -93,39 +101,21 @@ class TestAirtableDocumentDatabase:
         """
         It retrieves all rows of the documents table
         """
-        AirtableDocumentDatabase(
-            self.token,
-            self.base_id,
-            self.documents_table_id,
-            self.themes_table_id,
-        )
+        self.create_db()
         self.documents_table.all.assert_called()
 
     def test_airtable_api_gets_themes(self):
         """
-        It looks up theme data for the given theme ids
+        It retrieves all rows of the themes table
         """
-        AirtableDocumentDatabase(
-            self.token,
-            self.base_id,
-            self.documents_table_id,
-            self.themes_table_id,
-        )
-        assert self.themes_table.get.call_args_list == [
-            call("theme1_id"),
-            call("theme2_id"),
-        ]
+        self.create_db()
+        self.themes_table.all.assert_called()
 
     def test_get_all_documents_returns_documents(self):
         """
         get_all_documents returns all the documents in the table
         """
-        db = AirtableDocumentDatabase(
-            self.token,
-            self.base_id,
-            self.documents_table_id,
-            self.themes_table_id,
-        )
+        db = self.create_db()
         links = [doc["link"] for doc in db.get_all_documents()]
         assert "doc1" in links
         assert "doc2" in links
@@ -135,12 +125,7 @@ class TestAirtableDocumentDatabase:
         get_all_tags returns all the tags used in
         every document in the table
         """
-        db = AirtableDocumentDatabase(
-            self.token,
-            self.base_id,
-            self.documents_table_id,
-            self.themes_table_id,
-        )
+        db = self.create_db()
         tags = db.get_all_tags()
         assert "tag1" in tags
         assert "tag2" in tags
@@ -167,12 +152,7 @@ class TestAirtableDocumentDatabase:
         get_documents_for_tag returns all, and only,
         documents for the given tag
         """
-        db = AirtableDocumentDatabase(
-            self.token,
-            self.base_id,
-            self.documents_table_id,
-            self.themes_table_id,
-        )
+        db = self.create_db()
         links1 = [doc["link"] for doc in db.get_documents_for_tag("tag1")]
         links2 = [doc["link"] for doc in db.get_documents_for_tag("tag2")]
         links3 = [doc["link"] for doc in db.get_documents_for_tag("tag3")]
@@ -188,12 +168,7 @@ class TestAirtableDocumentDatabase:
         get_documents_for_theme returns all, and only,
         documents for the given theme
         """
-        db = AirtableDocumentDatabase(
-            self.token,
-            self.base_id,
-            self.documents_table_id,
-            self.themes_table_id,
-        )
+        db = self.create_db()
         links1 = [doc["link"] for doc in db.get_documents_for_theme("theme1")]
         links2 = [doc["link"] for doc in db.get_documents_for_theme("theme2")]
         assert "doc1" in links1
@@ -206,13 +181,7 @@ class TestAirtableDocumentDatabase:
         get_tags_for_theme returns all, and only, tags for
         documents with the given theme
         """
-        db = AirtableDocumentDatabase(
-            self.token,
-            self.base_id,
-            self.documents_table_id,
-            self.themes_table_id,
-        )
-
+        db = self.create_db()
         tags1 = db.get_tags_for_theme("theme1")
         tags2 = db.get_tags_for_theme("theme2")
 
@@ -225,12 +194,7 @@ class TestAirtableDocumentDatabase:
         """
         get_theme returns metadata for the passed theme
         """
-        db = AirtableDocumentDatabase(
-            self.token,
-            self.base_id,
-            self.documents_table_id,
-            self.themes_table_id,
-        )
+        db = self.create_db()
 
         theme1 = db.get_theme("theme1")
         theme2 = db.get_theme("theme2")
