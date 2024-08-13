0cbd7ca95cbca3512f556dd408344ebd7d0bb904
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-08-13 15:21:23 +0200

    refactor rag module to abstract out all the llamaindex plumbing

--- ddlh/rag/__init__.py
+++ ddlh/rag/__init__.py
@@ -1,27 +1,13 @@
 import re
 from collections import defaultdict
-from os import environ
 from typing import List, TypedDict, cast
 
-from llama_index.core import Document as LlamaDocument
-from llama_index.core import QueryBundle, VectorStoreIndex, get_response_synthesizer
 from llama_index.core.base.response.schema import Response
-from llama_index.core.ingestion import IngestionPipeline
-from llama_index.core.node_parser import SentenceSplitter
-from llama_index.core.response_synthesizers import BaseSynthesizer, ResponseMode
 from llama_index.core.schema import (
-    Node,
     NodeRelationship,
     NodeWithScore,
     RelatedNodeInfo,
-)
-from llama_index.embeddings.mistralai import MistralAIEmbedding  # type: ignore
-from llama_index.llms.mistralai import MistralAI  # type: ignore
-from llama_index.storage.docstore.elasticsearch import (  # type: ignore
-    ElasticsearchDocumentStore,
-)
-from llama_index.storage.kvstore.elasticsearch import (  # type: ignore
-    ElasticsearchKVStore,
+    TextNode,
 )
 
 from ddlh import airtable
@@ -32,21 +18,9 @@ from ddlh.models import (
     SearchResult,
     Summary,
 )
+from ddlh.rag import llamaindex
 from ddlh.repositories import DocumentsRepository
 from ddlh.utils import compact
-
-from llama_index.vector_stores.elasticsearch import (  # type: ignore # isort:skip
-    ElasticsearchStore as ElasticsearchVectorStore,
-)
-
-
-TOP_K: int = 10
-
-RESPONSE_MODE: ResponseMode = ResponseMode.REFINE
-
-EMBEDDING_MODEL_NAME: str = "mistral-embed"
-
-LLM_MODEL_NAME: str = "open-mixtral-8x22b"
 
 DOCUMENT_SUMMARY_PROMPT: str = """
 <s>[INST]explain in one sentence how this document relates to the theme of \"{query}\".
@@ -61,45 +35,15 @@ TOP_SENTENCE_PROMPT: str = """
 
 DocumentResult = TypedDict(
     "DocumentResult",
-    {"doc_id": str, "score": float, "document": Node, "results": List[NodeWithScore]},
+    {
+        "doc_id": str,
+        "score": float,
+        "document": TextNode,
+        "results": List[NodeWithScore],
+    },
 )
 
-QueryResponse = tuple[
-    list[Node], list[DocumentResult], list[tuple[DocumentResult, Response]], Response
-]
-
-
-def _vector_store() -> ElasticsearchVectorStore:
-    return ElasticsearchVectorStore(
-        es_url=environ["ELASTICSEARCH_URL"],
-        index_name=environ["ELASTICSEARCH_EMBEDDINGS_INDEX"],
-        vector_field="embedding",
-    )
-
-
-def _docstore() -> ElasticsearchDocumentStore:
-    return ElasticsearchDocumentStore(
-        elasticsearch_kvstore=ElasticsearchKVStore(
-            index_name=environ["ELASTICSEARCH_KV_INDEX"],
-            es_client=None,
-            es_url=environ["ELASTICSEARCH_URL"],
-        ),
-        node_collection_index=environ["ELASTICSEARCH_NODE_INDEX"],
-        ref_doc_collection_index=environ["ELASTICSEARCH_REF_DOC_INDEX"],
-        metadata_collection_index=environ["ELASTICSEARCH_METADATA_INDEX"],
-    )
-
-
-def _embedding_model() -> MistralAIEmbedding:
-    return MistralAIEmbedding(EMBEDDING_MODEL_NAME, api_key=environ["MISTRAL_API_KEY"])
-
-
-def _llm() -> MistralAI:
-    return MistralAI(LLM_MODEL_NAME, api_key=environ["MISTRAL_API_KEY"])
-
-
-def _response_synthesizer() -> BaseSynthesizer:
-    return get_response_synthesizer(llm=_llm(), response_mode=RESPONSE_MODE)
+la_rag = llamaindex.create_instance()
 
 
 def _collate_and_rerank_by_document_ids(
@@ -115,7 +59,7 @@ def _collate_and_rerank_by_document_ids(
         )
         doc_id = source_node.node_id
         try:
-            _docstore().get_document(doc_id)
+            la_rag.get_document(doc_id)
         except Exception:
             print("doc id %s not found!" % doc_id)
             continue
@@ -125,7 +69,7 @@ def _collate_and_rerank_by_document_ids(
             + (result.score or 0.0) / counter[doc_id]
         )
         if doc_id not in docs:
-            docs[doc_id] = _docstore().get_document(doc_id)
+            docs[doc_id] = la_rag.get_document(doc_id)
         results_by_doc_id[doc_id].append(result)
 
     return [
@@ -146,12 +90,12 @@ def _generate_document_summaries(
     for doc in sorted_docs:
         if len(responses) > 2:
             break
-        response = _response_synthesizer().synthesize(
+        response = la_rag.synthesize(
             DOCUMENT_SUMMARY_PROMPT.format(query=query),
             # nodes=[NodeWithScore(node=doc["document"], score=doc["score"])],
             nodes=doc["results"],
         )
-        if not re.search("(not |ir)relevant", response.response):
+        if response.response and not re.search("(not |ir)relevant", response.response):
             responses.append((doc, response))
     return responses
 
@@ -159,39 +103,16 @@ def _generate_document_summaries(
 def _generate_top_sentence(
     responses: list[tuple[DocumentResult, Response]], query: str
 ) -> Response:
-    return cast(
-        Response,
-        _response_synthesizer().synthesize(
-            TOP_SENTENCE_PROMPT.format(query=query),
-            nodes=[r2 for (d, r) in responses for r2 in d["results"]],
-        ),
+    return la_rag.synthesize(
+        TOP_SENTENCE_PROMPT.format(query=query),
+        nodes=[r2 for (d, _r) in responses for r2 in d["results"]],
     )
 
 
-def index_documents(documents: List[DocumentWithText]) -> None:
-    llama_documents = [
-        LlamaDocument(text=document.embeddable_text, doc_id=document.id)
-        for document in documents
-    ]
-    embedding = _embedding_model()
-    splitter = SentenceSplitter(chunk_size=350, chunk_overlap=50)
-    es_vector_store = _vector_store()
-    es_docstore = _docstore()
-    pipeline = IngestionPipeline(
-        transformations=[splitter, embedding],
-        vector_store=es_vector_store,
-        docstore=es_docstore,
-    )
-    pipeline.run(documents=llama_documents)
-
-
-def _query_to_sorted_docs(query: str) -> tuple[list[Node], List[DocumentResult]]:
-    embed_model = _embedding_model()
-    index = VectorStoreIndex.from_vector_store(_vector_store(), embed_model=embed_model)
-    index.storage_context.docstore = _docstore()
-    retriever = index.as_retriever(similarity_top_k=TOP_K)
-    bundle = QueryBundle(query, embedding=embed_model.get_query_embedding(query))
-    results = retriever.retrieve(bundle)
+def _query_to_sorted_docs(
+    query: str,
+) -> tuple[list[NodeWithScore], List[DocumentResult]]:
+    results = la_rag.query_documents(query)
     return (results, _collate_and_rerank_by_document_ids(results))
 
 
@@ -230,3 +151,7 @@ def query(
         documents=[d["doc_id"] for d in sorted_docs],
         summary=summary,
     )
+
+
+def index_documents(documents: List[DocumentWithText]) -> None:
+    la_rag.index_documents(documents)
---
+++ ddlh/rag/llamaindex.py
@@ -0,0 +1,133 @@
+from dataclasses import dataclass
+from os import environ
+from typing import Sequence, Union, cast
+
+from llama_index.core import Document as LlamaDocument
+from llama_index.core import QueryBundle, VectorStoreIndex, get_response_synthesizer
+from llama_index.core.base.response.schema import Response
+from llama_index.core.ingestion import IngestionPipeline
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core.response_synthesizers import ResponseMode
+from llama_index.core.schema import BaseNode, NodeWithScore, TextNode
+from llama_index.embeddings.mistralai import MistralAIEmbedding  # type: ignore
+from llama_index.llms.mistralai import MistralAI  # type: ignore
+from llama_index.storage.docstore.elasticsearch import (  # type: ignore
+    ElasticsearchDocumentStore,
+)
+from llama_index.storage.kvstore.elasticsearch import (  # type: ignore
+    ElasticsearchKVStore,
+)
+
+from ddlh.models import DocumentWithText
+
+from llama_index.vector_stores.elasticsearch import (  # type: ignore # isort:skip
+    ElasticsearchStore as ElasticsearchVectorStore,
+)
+
+AnyNode = Union[BaseNode, NodeWithScore]
+
+
+@dataclass
+class LlamaIndexConfig:
+    embedding_model_name: str
+    llm_model_name: str
+    es_url: str
+    es_embeddings_index: str
+    es_embeddings_field: str
+    es_kv_index: str
+    es_node_index: str
+    es_ref_doc_index: str
+    es_metadata_index: str
+    mistral_api_key: str
+    embedding_chunk_size: int
+    embedding_chunk_overlap: int
+    retrieval_top_k: int
+
+
+class LlamaIndex:
+    def __init__(self, config: LlamaIndexConfig):
+        vector_store = ElasticsearchVectorStore(
+            es_url=config.es_url,
+            index_name=config.es_embeddings_index,
+            vector_field=config.es_embeddings_field,
+        )
+        docstore = ElasticsearchDocumentStore(
+            elasticsearch_kvstore=ElasticsearchKVStore(
+                index_name=config.es_kv_index,
+                es_url=config.es_url,
+                es_client=None,
+            ),
+            node_collection_index=config.es_node_index,
+            ref_doc_collection_index=config.es_ref_doc_index,
+            metadata_collection_index=config.es_metadata_index,
+        )
+        embedding_model = MistralAIEmbedding(
+            config.embedding_model_name, api_key=config.mistral_api_key
+        )
+        llm = MistralAI(config.llm_model_name, api_key=config.mistral_api_key)
+        splitter = SentenceSplitter(
+            chunk_size=config.embedding_chunk_size,
+            chunk_overlap=config.embedding_chunk_overlap,
+        )
+        index = VectorStoreIndex.from_vector_store(
+            vector_store, embed_model=embedding_model
+        )
+        index.storage_context.docstore = docstore
+        response_synthesizer = get_response_synthesizer(
+            llm=llm,
+            response_mode=ResponseMode.REFINE,
+        )
+        pipeline = IngestionPipeline(
+            transformations=[splitter, embedding_model],
+            vector_store=vector_store,
+            docstore=docstore,
+        )
+        retriever = index.as_retriever(similarity_top_k=config.retrieval_top_k)
+
+        self.retriever = retriever
+        self.pipeline = pipeline
+        self.embedding_model = embedding_model
+        self.response_synthesizer = response_synthesizer
+        self.docstore = docstore
+        # Even though we don't explicitly use this again, we need to
+        # ensure we hold a reference to it, otherwise we get a
+        # "Timeout context manager should be used inside a task" RuntimeError:
+        self.vector_store = vector_store
+
+    def index_documents(self, documents: list[DocumentWithText]) -> None:
+        llama_documents = [
+            LlamaDocument(text=document.embeddable_text, doc_id=document.id)
+            for document in documents
+        ]
+        self.pipeline.run(documents=llama_documents)
+
+    def get_document(self, doc_id: str) -> TextNode:
+        return cast(TextNode, self.docstore.get_document(doc_id))
+
+    def synthesize(self, prompt: str, nodes: Sequence[AnyNode]) -> Response:
+        return cast(Response, self.response_synthesizer.synthesize(prompt, nodes=nodes))
+
+    def query_documents(self, query: str) -> list[NodeWithScore]:
+        bundle = QueryBundle(
+            query, embedding=self.embedding_model.get_query_embedding(query)
+        )
+        return cast(list[NodeWithScore], self.retriever.retrieve(bundle))
+
+
+def create_instance() -> LlamaIndex:
+    config = LlamaIndexConfig(
+        es_url=environ["ELASTICSEARCH_URL"],
+        es_embeddings_index=environ["ELASTICSEARCH_EMBEDDINGS_INDEX"],
+        es_kv_index=environ["ELASTICSEARCH_KV_INDEX"],
+        es_node_index=environ["ELASTICSEARCH_NODE_INDEX"],
+        es_ref_doc_index=environ["ELASTICSEARCH_REF_DOC_INDEX"],
+        es_metadata_index=environ["ELASTICSEARCH_METADATA_INDEX"],
+        mistral_api_key=environ["MISTRAL_API_KEY"],
+        es_embeddings_field=environ["ELASTICSEARCH_EMBEDDINGS_FIELD"],
+        embedding_model_name=environ["MISTRAL_EMBEDDING_MODEL_NAME"],
+        llm_model_name=environ["MISTRAL_LANGUAGE_MODEL_NAME"],
+        embedding_chunk_size=int(environ["EMBEDDING_CHUNK_SIZE"]),
+        embedding_chunk_overlap=int(environ["EMBEDDING_CHUNK_OVERLAP"]),
+        retrieval_top_k=int(environ["RETRIEVAL_TOP_K"]),
+    )
+    return LlamaIndex(config)
