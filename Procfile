a598f29fdcc8a7d88743e73fd36944500153d7a1
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-09-03 09:10:13 +0200

    heroku deployment config pt 1 of n

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: python -m ddlh.app
+worker: celery -A ddlh.celery worker -P gevent
--- ddlh/app.py
+++ ddlh/app.py
@@ -1,3 +1,5 @@
+from os import environ
+
 from gevent.pywsgi import WSGIServer
 
 from ddlh import create_app
@@ -6,7 +8,7 @@ app = create_app()
 
 
 def serve() -> None:
-    http_server = WSGIServer(("0.0.0.0", 5000), app)
+    http_server = WSGIServer(("0.0.0.0", int(environ.get("PORT", "5000"))), app)
     http_server.serve_forever()
 
 
